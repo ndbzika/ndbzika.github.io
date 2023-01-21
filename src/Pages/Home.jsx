79de5d3890138eda458b896e6fbd8644cf13be43
Flavio <flaviohenriquefc@gmail.com>
2023-01-20 22:45:10 -0300

    Alterando texto e scrollbar

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,7 +18,7 @@ export const Home = () => {
                 <Avatar alt='Flávio Henrique' src="https://thumbs2.imgbox.com/90/b9/0HtrPDN8_t.png" sx={{ width: 400, height: 400, flexGrow:0 }}/>
                 <Box maxWidth='sm' sx={{display:'flex', flexDirection:'column', justifyContent:'center',flexGrow:2, textAlign:'center', backgroundColor:'white', borderRadius:5, width: 300, height: 250}}>
                     <Typography variant='h1' component='h1' color='darkblue' sx={{fontWeight:'bold'}}>Flávio</Typography>
-                    <Typography component='h3' variant='h4' color='darkblue'>Estudante, Programador e um Desenvolver Feliz</Typography>
+                    <Typography component='h3' variant='h4' color='darkblue' fontStyle='italic'>"Não basta conquistar a sabedoria, é preciso usá-la." <Typography>~ Cícero</Typography></Typography>
                 </Box>
             </Container>
         </section>
@@ -27,7 +27,7 @@ export const Home = () => {
             <Box maxWidth='xl' sx={{width:'100%', backgroundColor:'white'}}>
                 &nbsp;
                 <Typography variant='h2' component='div' textAlign='center' className='about-me-titulo' sx={{ fontWeight:'bold'}}>Quem é Flávio?</Typography>
-                <Typography variant='h5' component='p' textAlign='center' className='about-me-texto'>Olá, meu nome é Flávio Henrique e atualmente estudo no IFPB campus Cajazeiras e curso Análise e Desenvolvimento de Sistemas. Entusiasta de Front-End e amante da aprendizagem, gosto de me desafiiar e colocar metas para que possa evoluir cada dia mais meus conhecimentos e meu eu interior, por exemplo esse site feito com ReactJs e SCSS que aprendi recentemente. </Typography>
+                <Typography variant='h5' component='p' textAlign='center' className='about-me-texto'>Olá, meu nome é Flávio Henrique e atualmente curso Análise e Desenvolvimento de Sistemas no IFPB campus Cajazeiras. Entusiasta de Front-End e amante da aprendizagem, gosto de desafios evoluir cada dia mais meus conhecimentos.</Typography>
                 &nbsp;
             </Box>
         </section>
