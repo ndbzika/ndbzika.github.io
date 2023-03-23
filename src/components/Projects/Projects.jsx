dcaba9cd90ea3c9725ce6dd085470d85ffcd8eb5
Flavio Henrique <chakcpps@gmail.com>
2023-03-22 22:47:53 -0300

    Componentizando repositórios

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,27 +1,15 @@
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardMedia,
-  Typography,
-} from '@mui/material';
 import React, { useEffect, useState, useRef } from 'react';
+
+import { Repositories } from '../Repositories/index';
 import { motion } from 'framer-motion';
 
 const ProjectsSection = () => {
-  const [repositories, setRepositories] = useState([]);
   const [width, setWidth] = useState(0);
   const carousel = useRef();
 
   useEffect(() => {
     setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
   }, [carousel.current?.scrollWidth]);
-
-  useEffect(() => {
-    fetch('https://api.github.com/users/ndbzika/repos')
-      .then((response) => response.json())
-      .then((data) => setRepositories(data));
-  }, []);
   return (
     <>
       <motion.div
@@ -34,36 +22,7 @@ const ProjectsSection = () => {
           drag="x"
           dragConstraints={{ right: 0, left: -width }}
         >
-          {repositories.map((repository) => {
-            return (
-              <Card
-                sx={{ minWidth: 350, maxHeight: 300 }}
-                key={repository.keys_url}
-              >
-                <a
-                  href={repository.html_url}
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ textDecoration: 'none', color: 'black' }}
-                >
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="100"
-                      image={repository.owner.avatar_url}
-                      alt="calculadora"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {repository.name}
-                      </Typography>
-                      <Typography>{repository.description}</Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </a>
-              </Card>
-            );
-          })}
+          <Repositories />
         </motion.div>
       </motion.div>
     </>
