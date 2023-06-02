1f6e4847302655dcb2fbcc968f915f3caae69e15
ndbzika <chakcpps@gmail.com>
2023-06-01 22:45:45 -0300

    view: Criando navbar

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { Box, HStack } from '@chakra-ui/react'
+import {BsLinkedin} from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import Link from 'next/link'
+
+import styles from './styles.module.scss'
+
+export const Navbar = () => {
+  return (
+    <Box className={styles.container} h='75px'>
+
+      <HStack className={styles.links}>
+        <Link href='/#home' scroll={false}>Home</Link>
+
+        <Link href='/#projects' scroll={false}>Projects</Link>
+
+        <Link href='/#skills' scroll={false}>Skills</Link>
+      </HStack>
+
+      <HStack gap='35px'>
+        <BsGithub className={styles.icon} size='1.8em' />
+        <BsLinkedin className={styles.icon} size='1.8em' />
+      </HStack>
+
+
+    </Box>
+  )
+}
